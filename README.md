0b68375a438eb0e74ecbf720429091311bf92fe5
Gabriel M <gabrielpdmello@gmail.com>
2025-08-11 22:46:05 -0300

    Deal with city not found

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ Simple project to learn how to use promises, await/async and APIs.
 
 # Todo
 - cool UI
-- add a way do deal with city not found
 - add more features (6-day forecast, etc)
 - refactor and use webpack
--- script.js
+++ script.js
@@ -9,39 +9,46 @@ const minmaxtemp = document.querySelector(".minmaxtemp");
 const conditions = document.querySelector(".conditions");
 const unit = document.querySelector(".unit")
 const loading = document.querySelector(".loading");
+const errorMessage = document.querySelector(".error");
 
 
 async function getLocationTemp(city){
     loading.classList.remove("hide");
+    errorMessage.textContent = "";
 
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=WSTTFR7JHD25RXA4U5PVLCKWJ`, {mode: 'cors'});
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=WSTTFR7JHD25RXA4U5PVLCKWJ`, {mode: 'cors'});
+        const weatherData = await response.json();
 
-    const weatherData = await response.json();
+        resolvedAddress.textContent = `${weatherData.resolvedAddress}`;
+        conditions.textContent = `${weatherData.currentConditions.conditions}`;
+        if (unit.value === "fahrenheit") {
+            temp.textContent = `Temperature: ${weatherData.currentConditions.temp}° F`;
+            minmaxtemp.textContent = `${weatherData.days[0].tempmin} | ${weatherData.days[0].tempmax}° F`;
+        } else {
+            temp.textContent = `Temperature: ${Math.round((weatherData.currentConditions.temp -32) / 1.8 * 10) / 10}° C`;
+            minmaxtemp.textContent = `${Math.round((weatherData.days[0].tempmin -32) / 1.8 * 10) / 10}° | ${Math.round((weatherData.days[0].tempmax -32) / 1.8 * 10) / 10}° C`;
+        }
 
-    resolvedAddress.textContent = `${weatherData.resolvedAddress}`;
-    conditions.textContent = `${weatherData.currentConditions.conditions}`;
-    if (unit.value === "fahrenheit") {
-        temp.textContent = `Temperature: ${weatherData.currentConditions.temp}° F`;
-        minmaxtemp.textContent = `${weatherData.days[0].tempmin} | ${weatherData.days[0].tempmax}° F`;
-    } else {
-        temp.textContent = `Temperature: ${Math.round((weatherData.currentConditions.temp -32) / 1.8 * 10) / 10}° C`;
-        minmaxtemp.textContent = `${Math.round((weatherData.days[0].tempmin -32) / 1.8 * 10) / 10}° | ${Math.round((weatherData.days[0].tempmax -32) / 1.8 * 10) / 10}° C`;
+        body.removeAttribute("class");
+
+        if (weatherData.currentConditions.temp > 86) {
+            body.classList.add("hot");
+        } else if (weatherData.currentConditions.temp > 59) {
+            body.classList.add("warm");
+        } else if (weatherData.currentConditions.temp > 41) {
+            body.classList.add("cold");
+        } else {
+            body.classList.add("ice-cold");
+        }
+
+        loading.classList.add("hide");
+        weatherResults.classList.remove("hide");
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        errorMessage.textContent = error + " (you probably typed the city wrong, u dumdum)";
+        loading.classList.add("hide");
     }
-
-    body.removeAttribute("class");
-
-    if (weatherData.currentConditions.temp > 86) {
-        body.classList.add("hot");
-    } else if (weatherData.currentConditions.temp > 59) {
-        body.classList.add("warm");
-    } else if (weatherData.currentConditions.temp > 41) {
-        body.classList.add("cold");
-    } else {
-        body.classList.add("ice-cold");
-    }
-
-    loading.classList.add("hide");
-    weatherResults.classList.remove("hide");
 }
 
 submitBtn.addEventListener("click", () => {
